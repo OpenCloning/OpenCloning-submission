3c7f997043b03b49b24263427de34edaa9a2c4d3
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-05-30 14:58:59 +0100

    small change

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing import Optional
-
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse, HTMLResponse
-
+from fastapi.staticfiles import StaticFiles
+from scrape import scrape_addgene_kit
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
@@ -17,6 +19,23 @@ async def root_form(blah: str = Form(...)):
     # Get form data
     print(blah)
     return FileResponse("bye.html")
+
+
+@app.get("/get_kit_info")
+async def get_kit_info():
+    return FileResponse("get_kit_info.html")
+
+
+# Takes a URL and returns the HTML content of the page
+@app.get("/scrape_addgene/")
+async def scrape_addgene(url: str):
+    if not url.startswith("https://www.addgene.org/"):
+        raise HTTPException(status_code=400, detail="URL must be from Addgene")
+    try:
+        page_content = await scrape_addgene_kit(url)
+        return HTMLResponse(page_content)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @app.get("/items/{item_id}")
---
+++ scrape.py
@@ -0,0 +1,54 @@
+from playwright.async_api import async_playwright
+
+
+async def scrape_addgene_kit(url: str) -> str:
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        my_page = await browser.new_page()
+        # Change the URL to the site you want for your Playwright web scraping project
+        await my_page.goto(url)
+        await my_page.wait_for_load_state("load")
+
+        # Error if #kit-contents-link is not found
+        if not await my_page.query_selector("#kit-contents-link"):
+            raise Exception(
+                "Problem scraping AddGene website:'Kit contents' tab not found"
+            )
+
+        # Click on contents link
+        await my_page.click("#kit-contents-link")
+        await my_page.wait_for_load_state("load")
+
+        # Error if the select element is not found
+        if not await my_page.query_selector("label:has-text('Show ') >> select"):
+            raise Exception("Problem scraping AddGene website: plasmid table not found")
+
+        # Error if the option "All" is not found
+        if not await my_page.query_selector(
+            "label:has-text('Show ') >> select >> option[value='All']"
+        ):
+            raise Exception(
+                "Problem scraping AddGene website: option 'All' not found in the table of plasmids"
+            )
+
+        # Click on a select element inside a label
+        # the label contains text "Show "
+        # include label in the query
+        # Select option "All"
+        await my_page.select_option("label:has-text('Show ') >> select", "All")
+        await my_page.wait_for_load_state("load")
+
+        # Get content
+        page_content = await my_page.content()
+
+        # Close the browser
+        await browser.close()
+
+    return page_content
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    url = "https://www.addgene.org/kits/sieber-moclo-pichia-toolkit/"
+    print(asyncio.run(scrape_addgene_kit(url)))
