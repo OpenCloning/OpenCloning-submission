31ea7a3c0e62d7de0e784f675bd81d77629e1c30
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-06-07 19:22:28 +0100

    Docs 2 (#39)
    * update_index updated
    
    * added docs
    
    * added docs
    
    * fixed docs

--- update_index.py
+++ update_index.py
@@ -9,7 +9,10 @@ for submission in all_submissions:
     with open(submission) as f:
         submission_dict = json.load(f)
 
-    index[submission.split("/")[-2]] = submission_dict["kit"]
+    submission_folder = submission.split("/")[-2]
+    index[submission_folder] = dict()
+    index[submission_folder]["kit"] = submission_dict["kit"]
+    index[submission_folder]["assemblies"] = submission_dict["assemblies"]
 
 with open("index.json", "w") as f:
     json.dump(index, f, indent=2)
