5cb760ff5c212b59c12d20485f109f70c1491911
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-07-28 19:44:31 +0200

    include GoldenPiCS

--- re_process.py
+++ re_process.py
@@ -3,6 +3,13 @@ import glob
 import os
 import json
 from copy import deepcopy
+from opencloning.pydantic_models import (
+    BaseCloningStrategy as CloningStrategy,
+    HomologousRecombinationSource,
+    GenomeCoordinatesSource,
+    SourceInput,
+)
+from opencloning_linkml.datamodel.models import TemplateSequence
 
 submission_folders = [d for d in glob.glob("submissions/*") if os.path.isdir(d)]
 
@@ -46,6 +53,45 @@ for old_category, new_category in mappings:
     for option in old_options:
         option["info"]["well"] = "Base CIDAR Kit " + option["info"]["well"]
     source_new["options"].extend(old_options)
+
+
+## Add Homologous Recombination to K. phaffi kit
+files = [
+    "processed/kits-gasser-goldenpics/templates/assembly_template_001.json",
+    "processed/kits-gasser-goldenpics/templates/assembly_template_003.json",
+]
+
+for file in files:
+    with open(file, "r") as f:
+        template = json.load(f)
+    cs = CloningStrategy.model_validate(template)
+    restriction_source = next(
+        s for s in cs.sources if s.type == "RestrictionAndLigationSource"
+    )
+
+    genome_source = GenomeCoordinatesSource(
+        id=0,
+        assembly_accession="GCA_900235035.2",
+        sequence_accession="LT962479.2",
+        locus_tag="BQ9382_C4-0695",
+        start=1237975,
+        end=241966,
+        strand=1,
+    )
+
+    cs.add_source_and_sequence(genome_source, TemplateSequence(circular=False, id=0))
+
+    hom_rec = HomologousRecombinationSource(
+        id=0,
+        input=[
+            SourceInput(sequence=genome_source.id),
+            SourceInput(sequence=restriction_source.id),
+        ],
+    )
+    cs.add_source_and_sequence(hom_rec, TemplateSequence(circular=False, id=0))
+
+    with open(file, "w") as f:
+        json.dump(cs.model_dump(), f, indent=2)
 
 with open(extension_path, "w") as f:
     json.dump(template_extension, f, indent=2)
